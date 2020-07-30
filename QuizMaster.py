c181cdf56f6afc8ebc954332b4478cad2577682c
Frederic Reinhardt <nonooby@gmx.net>
2020-07-30 21:08:00 +0200

    refactored lang GER => EN

--- QuizMaster.py
+++ QuizMaster.py
@@ -221,54 +221,54 @@ def cleanHtmlResult():
 
 
 # printEvaluatedResult
-# prints the Result depending on the amount of matches (for exact and estimated matches)
+# prints the Result depending on the amount of matches (for exact and approximate matches)
 # the function will print additional notifications if the question is negated
 def printEvaluatedResult():
     if exakteTreffer_A > 0:
-        print(f'\n A - EXAKTE TREFFER | {AntwortAText}:  {exakteTreffer_A}x  ||  {exakteTreffer_ListA[:5]}')
+        print(f'\n A - EXAKTE TREFFER | {AnswerAText}:  {exakteTreffer_A}x  ||  {listA_exactMatches[:5]}')
 
     if exakteTreffer_B > 0:
-        print(f'\n B - EXAKTE TREFFER | {AntwortBText}:  {exakteTreffer_B}x  ||  {exakteTreffer_ListB[:5]}')
+        print(f'\n B - EXAKTE TREFFER | {AnswerBText}:  {exakteTreffer_B}x  ||  {listB_exactMatches[:5]}')
 
     if exakteTreffer_C > 0:
-        print(f'\n C - EXAKTE TREFFER | {AntwortCText}:  {exakteTreffer_C}x  ||  {exakteTreffer_ListC[:5]}')
+        print(f'\n C - EXAKTE TREFFER | {AnswerCText}:  {exakteTreffer_C}x  ||  {listC_exactMatches[:5]}')
 
     if ((exakteTreffer_A == 0) and (exakteTreffer_B == 0) and (exakteTreffer_C == 0)):
-        if ungefaehreTreffer_A > 0:
-            print(f'\n A - ungefähr | {Asplit}:  {ungefaehreTreffer_A}x  ||  {ungefaehreTreffer_ListA[:5]}')
+        if approximateMatches_A > 0:
+            print(f'\n A - ungefähr | {Asplit}:  {approximateMatches_A}x  ||  {listA_approximateMatches[:5]}')
 
-        if ungefaehreTreffer_B > 0:
-            print(f'\n B - ungefähr | {Bsplit}:  {ungefaehreTreffer_B}x  ||  {ungefaehreTreffer_ListB[:5]}')
+        if approximateMatches_B > 0:
+            print(f'\n B - ungefähr | {Bsplit}:  {approximateMatches_B}x  ||  {listB_approximateMatches[:5]}')
 
-        if ungefaehreTreffer_C > 0:
-            print(f'\n C - ungefähr | {Csplit}:  {ungefaehreTreffer_C}x  ||  {ungefaehreTreffer_ListC[:5]}')
+        if approximateMatches_C > 0:
+            print(f'\n C - ungefähr | {Csplit}:  {approximateMatches_C}x  ||  {listC_approximateMatches[:5]}')
 
-    if "nicht" in FrageText:  # Abfrage ob Frage negiert wird
+    if "nicht" in QuestionText:  # Abfrage ob Frage negiert wird
         print('\nACHTUNG: Das Wort "nicht" wurde gefunden - GGF JOKER VERWENDEN')
 
-    if "kein" in FrageText:  # Abfrage ob Frage negiert wird
+    if "kein" in QuestionText:  # Abfrage ob Frage negiert wird
         print('\nACHTUNG: Das Wort "kein" wurde gefunden - GGF JOKER VERWENDEN')
 
-    if "no" in FrageText:  # Condition: Question was negated
+    if "no" in QuestionText:  # Condition: Question was negated
         print('\nWARNING: The Question contains the Word "no" - Result may be irritating ')
 
-    if "not" in FrageText:  # Condition: Question was negated
+    if "not" in QuestionText:  # Condition: Question was negated
         print('\nWARNING: The Question contains the Word "not" - Result may be irritating ')
 
 
 # SearchEngineUrls
 # defines the URLS for the AnswerA,B,C and the question
 def defineSearchEngineUrls():
-    urlA = searchEngineUrl + AntwortAText
-    urlB = searchEngineUrl + AntwortBText
-    urlC = searchEngineUrl + AntwortCText
-    urlF = searchEngineUrl + FrageTextReduziert + '&num=' + amount_searchResults
+    urlA = searchEngineUrl + AnswerAText
+    urlB = searchEngineUrl + AnswerBText
+    urlC = searchEngineUrl + AnswerCText
+    urlF = searchEngineUrl + QuestionTextReduced + '&num=' + amount_searchResults
     return urlA, urlB, urlC, urlF
 
 
 # initialize V A R I A B L E S -------------------------------------------------
 
-wahrscheinlichkeitenDictionary = {}
+percentageProbabilityDictionary = {} # contains the probability in percent that the answer is correct # todo unused
 
 # NAD = nummernAusgeschriebenDictionary
 # a list that contains the numbers and also the words in a range from 0-20. it is used for the results, because lower value numbers are often written as words.
@@ -284,58 +284,56 @@ amount_searchResults = '30'
 
 # C O R E   L O G I C  ---------------------------------------------------------
 
-AntwortAText = getAnswerA()
-AntwortBText = getAnswerB()
-AntwortCText = getAnswerC()
-FrageText, FrageTextReduziert = getQuestion()
+AnswerAText = getAnswerA()
+AnswerBText = getAnswerB()
+AnswerCText = getAnswerC()
+QuestionText, QuestionTextReduced = getQuestion()
 
 urlA, urlB, urlC, urlF = defineSearchEngineUrls()
 
-pyperclip.copy(FrageTextReduziert)  # copies the question text into the clipboard
-
+pyperclip.copy(QuestionTextReduced)  # copies the question text into the clipboard
 resultText = getResultViaRequest()
 cleanedResult = cleanHtmlResult()
-
-ergebnisliste = cleanedResult.split()  # splits multiple results into a word-list
-print(FrageText + '\n')  # print the question
+results = cleanedResult.split()  # splits multiple results into a word-list
+print(QuestionText + '\n')  # print the question
 
 # prints the answer
 if debugResult == '1':
     print(cleanedResult)
-    print(AntwortAText)
-    print(AntwortBText)
-    print(AntwortCText)
+    print(AnswerAText)
+    print(AnswerBText)
+    print(AnswerCText)
 
 # get list of exact matches for A, B and C
-exakteTreffer_ListA, ungefaehreTreffer_A, Asplit, ungefaehreTreffer_ListA = AnswerFunctions.answerA(AntwortAText,
-                                                                                                    AntwortBText,
-                                                                                                    AntwortCText,
-                                                                                                    cleanedResult,
-                                                                                                    ergebnisliste,
-                                                                                                    mratioDifferenceFloat,
-                                                                                                    spelledOutNumbersDictionary,
-                                                                                                    wahrscheinlichkeitenDictionary)
-exakteTreffer_ListB, ungefaehreTreffer_B, Bsplit, ungefaehreTreffer_ListB = AnswerFunctions.answerB(AntwortAText,
-                                                                                                    AntwortBText,
-                                                                                                    AntwortCText,
-                                                                                                    cleanedResult,
-                                                                                                    ergebnisliste,
-                                                                                                    mratioDifferenceFloat,
-                                                                                                    spelledOutNumbersDictionary,
-                                                                                                    wahrscheinlichkeitenDictionary)
-exakteTreffer_ListC, ungefaehreTreffer_C, Csplit, ungefaehreTreffer_ListC = AnswerFunctions.answerC(AntwortAText,
-                                                                                                    AntwortBText,
-                                                                                                    AntwortCText,
-                                                                                                    cleanedResult,
-                                                                                                    ergebnisliste,
-                                                                                                    mratioDifferenceFloat,
-                                                                                                    spelledOutNumbersDictionary,
-                                                                                                    wahrscheinlichkeitenDictionary)
+listA_exactMatches, approximateMatches_A, Asplit, listA_approximateMatches = AnswerFunctions.answerA(AnswerAText,
+                                                                                                     AnswerBText,
+                                                                                                     AnswerCText,
+                                                                                                     cleanedResult,
+                                                                                                     results,
+                                                                                                     mratioDifferenceFloat,
+                                                                                                     spelledOutNumbersDictionary,
+                                                                                                     percentageProbabilityDictionary)
+listB_exactMatches, approximateMatches_B, Bsplit, listB_approximateMatches = AnswerFunctions.answerB(AnswerAText,
+                                                                                                     AnswerBText,
+                                                                                                     AnswerCText,
+                                                                                                     cleanedResult,
+                                                                                                     results,
+                                                                                                     mratioDifferenceFloat,
+                                                                                                     spelledOutNumbersDictionary,
+                                                                                                     percentageProbabilityDictionary)
+listC_exactMatches, approximateMatches_C, Csplit, listC_approximateMatches = AnswerFunctions.answerC(AnswerAText,
+                                                                                                     AnswerBText,
+                                                                                                     AnswerCText,
+                                                                                                     cleanedResult,
+                                                                                                     results,
+                                                                                                     mratioDifferenceFloat,
+                                                                                                     spelledOutNumbersDictionary,
+                                                                                                     percentageProbabilityDictionary)
 
 # get amount of exact matches for A, B and C
-exakteTreffer_A = len(exakteTreffer_ListA)
-exakteTreffer_B = len(exakteTreffer_ListB)
-exakteTreffer_C = len(exakteTreffer_ListC)
+exakteTreffer_A = len(listA_exactMatches)
+exakteTreffer_B = len(listB_exactMatches)
+exakteTreffer_C = len(listC_exactMatches)
 
 printEvaluatedResult()
 
